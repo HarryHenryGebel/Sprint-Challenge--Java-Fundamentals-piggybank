743b92a7bddea629d67e1f21926c83e3399bee79
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 12:04:57 -0400

    Initialize CoinRepository

--- piggy-bank/src/main/java/tech/gebel/piggybank/controllers/CoinController.java
+++ piggy-bank/src/main/java/tech/gebel/piggybank/controllers/CoinController.java
@@ -1,2 +1,11 @@
-package tech.gebel.piggybank.controllers;public class CoinController {
+package tech.gebel.piggybank.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import tech.gebel.piggybank.repositories.CoinRepository;
+
+@RestController
+public class CoinController {
+    @Autowired
+    private CoinRepository coinRepository;
 }
