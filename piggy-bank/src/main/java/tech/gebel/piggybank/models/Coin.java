e526ceb69e74e6f2002860664c41267d8bec3b51
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 12:35:42 -0400

    Remove stray bracket

--- piggy-bank/src/main/java/tech/gebel/piggybank/models/Coin.java
+++ piggy-bank/src/main/java/tech/gebel/piggybank/models/Coin.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "coins")
 public class Coin {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)}
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long coinid;
     private String name;
     private String nameplural;
