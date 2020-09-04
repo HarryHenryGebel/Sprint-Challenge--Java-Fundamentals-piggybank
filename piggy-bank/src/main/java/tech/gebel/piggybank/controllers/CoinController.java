09676c4a731d680d5beedc99958dd229f144b9e1
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 12:35:57 -0400

    Complete total endpoint

--- piggy-bank/src/main/java/tech/gebel/piggybank/controllers/CoinController.java
+++ piggy-bank/src/main/java/tech/gebel/piggybank/controllers/CoinController.java
@@ -1,11 +1,34 @@
 package tech.gebel.piggybank.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.gebel.piggybank.models.Coin;
 import tech.gebel.piggybank.repositories.CoinRepository;
+
+import java.util.ArrayList;
 
 @RestController
 public class CoinController {
     @Autowired
     private CoinRepository coinRepository;
+
+    @GetMapping(value = "/total", produces = {"application/json"})
+    private ResponseEntity<?> displayTotals() {
+        Iterable<Coin> coins = coinRepository.findAll();
+
+        double totalValue = 0.00;
+        for (Coin coin : coins) {
+            totalValue += coin.getValue();
+            if (coin.getQuantity() == 1)
+                System.out.println(coin.getQuantity() + " " + coin.getName());
+            else
+                System.out.println(coin.getQuantity() + " " + coin.getNameplural());
+        }
+
+        System.out.println("The piggy bank holds " + totalValue);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
