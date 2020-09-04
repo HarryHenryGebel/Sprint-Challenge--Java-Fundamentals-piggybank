a98360259e4d4b0470972311e9922d8ced293683
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-04 11:55:48 -0400

    Coin constructors

--- piggy-bank/src/main/java/tech/gebel/piggybank/models/Coin.java
+++ piggy-bank/src/main/java/tech/gebel/piggybank/models/Coin.java
@@ -12,4 +12,14 @@ public class Coin {
     private String nameplural;
     private double value;
     private long quantity;
+
+    public Coin(String name, String nameplural, double value, long quantity) {
+        this.name = name;
+        this.nameplural = nameplural;
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public Coin() {
+    }
 }
